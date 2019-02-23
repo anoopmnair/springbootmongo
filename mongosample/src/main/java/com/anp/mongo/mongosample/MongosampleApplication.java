ac51f5b805f98a76dea94295e8a147adfc3b0f99
Anoop <anooppm.in@gmail.com>
2019-02-23 19:53:10 +0530

    initial commit

---
+++ mongosample/src/main/java/com/anp/mongo/mongosample/MongosampleApplication.java
@@ -0,0 +1,13 @@
+package com.anp.mongo.mongosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongosampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongosampleApplication.class, args);
+	}
+
+}
